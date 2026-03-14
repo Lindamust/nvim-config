ba11460b963b1d8d0fd8a4cda4832043dc304b67
Lindamust <208328889+Lindamust@users.noreply.github.com>
2026-03-14 12:29:19 +1100

    Add: Treesitter

--- init.lua
+++ init.lua
@@ -515,19 +515,81 @@ vim.pack.add({
   "https://www.github.com/ibhagwan/fzf-lua",
   "https://www.github.com/nvim-tree/nvim-tree.lua",
   "https://www.github.com/lewis6991/gitsigns.nvim",
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+  },
 })
 
 local function packadd(name)
   vim.cmd("packadd " .. name)
 end
 
+packadd("nvim-treesitter")
 packadd("mini.nvim")
 packadd("fzf-lua")
 packadd("nvim-tree.lua")
 packadd("gitsigns.nvim")
+
 -- ===============================================
 --                  PLUGIN CONFIGS
 -- ===============================================
+
+local setup_treesitter = function()
+  local treesitter = require("nvim-treesitter")
+  treesitter.setup({})
+  local ensure_installed = {
+    "vim",
+    "vimdoc",
+    "rust",
+    "c",
+    "cpp",
+    "go",
+    "html",
+    "css",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "python",
+    "typescript",
+    "vue",
+    "svelte",
+    "bash",
+    "toml",
+  }
+
+  local config = require("nvim-treesitter.config")
+
+  local already_installed = config.get_installed()
+  local parsers_to_install = {}
+
+  for _, parser in ipairs(ensure_installed) do
+    if not vim.tbl_contains(already_installed, parser) then
+      table.insert(parsers_to_install, parser)
+    end
+  end
+
+  if #parsers_to_install > 0 then
+    treesitter.install(parsers_to_install)
+  end
+
+  local group = vim.api.nvim_create_augroup("TreeSitterConfig", { clear = true })
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    callback = function(args)
+      if vim.list_contains(
+        treesitter.get_installed(),
+        vim.treesitter.language.get_lang(args.match))
+      then
+        vim.treesitter.start(args.buf)
+      end
+    end,
+  })
+end
+
+setup_treesitter()
 
 require("nvim-tree").setup({
   view = { width = 35 },
